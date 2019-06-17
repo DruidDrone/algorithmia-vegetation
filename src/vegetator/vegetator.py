717e74e4f3df1bee7ca6ace41b74b483be5f5f9a
Phil Stubbings <phil@parasec.net>
2019-06-17 18:27:43 +0100

    .

--- src/vegetator/vegetator.py
+++ src/vegetator/vegetator.py
@@ -44,4 +44,4 @@ class Vegetator():
         files = [get_datafile(f) for f in src] if type(src) is list else self.pre_processing(src).files()
         file_name = lambda file_str: file_str[file_str.rfind("/")+1:]
         #return {file_name(img_loc.getName()):self.post_processing(img_loc) for img_loc in files}
-        return dict(zip([file_name(f) for f in self.client.dir(src).files()], [self.post_processing(img_loc) for img_loc in files]))
+        return dict(zip([file_name(f.getName()) for f in self.client.dir(src).files()], [self.post_processing(img_loc) for img_loc in files]))
